4cb543541f197d31b164f4f49a4e55e5d33e8b43
Felix Rusu <felix@lowpowerlab.com>
2021-02-16 14:00:37 -0500

    Update idleWakePeriodic.ino

--- Examples/idleWakePeriodic/idleWakePeriodic.ino
+++ Examples/idleWakePeriodic/idleWakePeriodic.ino
@@ -12,18 +12,24 @@ void loop()
   // Each microcontroller comes with different number of peripherals
   // Comment off line of code where necessary
 
-  // ATmega328P, ATmega168
+
+#if defined (__AVR_ATmega328P__) || defined (__AVR_ATmega168__) 
   LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, 
                 SPI_OFF, USART0_OFF, TWI_OFF);
-
+#elif defined __AVR_ATmega644P__ || defined (__AVR_ATmega1284P__)
+  LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF,
+    TIMER1_OFF, TIMER0_OFF, SPI_OFF,
+    USART1_OFF, USART0_OFF, TWI_OFF);
+#endif
+               
   // ATmega32U4
   //LowPower.idle(SLEEP_8S, ADC_OFF, TIMER4_OFF, TIMER3_OFF, TIMER1_OFF, 
-  //		  TIMER0_OFF, SPI_OFF, USART1_OFF, TWI_OFF, USB_OFF);
+  //      TIMER0_OFF, SPI_OFF, USART1_OFF, TWI_OFF, USB_OFF);
 
   // ATmega2560
   //LowPower.idle(SLEEP_8S, ADC_OFF, TIMER5_OFF, TIMER4_OFF, TIMER3_OFF, 
-  //		  TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART3_OFF, 
-  //		  USART2_OFF, USART1_OFF, USART0_OFF, TWI_OFF);
+  //      TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART3_OFF, 
+  //      USART2_OFF, USART1_OFF, USART0_OFF, TWI_OFF);
 
   // ATmega256RFR2
   //LowPower.idle(SLEEP_8S, ADC_OFF, TIMER5_OFF, TIMER4_OFF, TIMER3_OFF, 
