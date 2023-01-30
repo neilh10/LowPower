d1b981cdc993ebd9c0c09d27cfd36e15f2bca692
neilh20 <neilh20+gitk1903@wLLw.net>
2023-01-30 09:06:26 -0800

    formatting change

--- Examples/lowpowerRtcSAMD51/lowpowerRtcSAMD51.cpp
+++ Examples/lowpowerRtcSAMD51/lowpowerRtcSAMD51.cpp
@@ -82,7 +82,7 @@ extern const String build_ref = "a\\" __FILE__ " " __DATE__ " " __TIME__ " ";
 // platformio  none default - 120MHz
 // board_build.f_cpu= 48000000L for 48MHz
 #if F_CPU >= 48000000L
-#if defined USBCON & !defined( PRINT_WITH_TXRX )
+#if defined USBCON & !defined(PRINT_WITH_TXRX)
 #define RUN_WITH_USB 1
 #endif // USBCON
 #endif //F_CPU
@@ -90,160 +90,160 @@ extern const String build_ref = "a\\" __FILE__ " " __DATE__ " " __TIME__ " ";
 #if defined PRINT_WITH_TXRX
 #define SerialTty Serial1
 //#undef RUN_WITH_USB
-#elif defined  RUN_WITH_USB
+#elif defined RUN_WITH_USB
 #define SerialTty Serial
 #else
 #define SerialTty Serial
 #endif // PRINT_WITH_TXRX
 
-
-// Internal Physical RTC and definitions 
+// Internal Physical RTC and definitions
 RTC_SAMD51 rtcIntPhy;
 #define RTC_ALM_ID 0
 #define ALARM_UPDATE_SEC 0
-bool alarmUpdate_sema=false;
+bool alarmUpdate_sema = false;
 
 #define COUNT_DOWN 5
-int count; 
+int count;
 
 // Forward references
 void lowpower_disable_ints(void);
 void lowpower_enable_ints(void);
-void flash_redLed(int count,int space_ms);
+void flash_redLed(int count, int space_ms);
 void print_mclk(void);
-void print_rtc_time_field(uint32_t time_value);     
+void print_rtc_time_field(uint32_t time_value);
 void alarmMatch(uint32_t flag);
 void updateAlarm(int update_secs);
-
 
 void setup()
 {
   //Simple user interface
-  pinMode(LED1 , OUTPUT);
-  digitalWrite(LED1 , HIGH);
+  pinMode(LED1, OUTPUT);
+  digitalWrite(LED1, HIGH);
 
-  #if PRINT_DETAILS > 0
-	// Wait for serial (possibly USB) port to open
+#if PRINT_DETAILS > 0
+  // Wait for serial (possibly USB) port to open
   SerialTty.begin(SERIAL_TTY_BAUD);
-  #if defined RUN_WITH_USB
+#if defined RUN_WITH_USB
   delay(100);
   count = 500;
-	while(!SerialTty) {
+  while (!SerialTty)
+  {
     delay(100);
-    if (count-- < 0) break;
-    digitalWrite(LED1 , count&0x01);
-    };
+    if (count-- < 0)
+      break;
+    digitalWrite(LED1, count & 0x01);
+  };
   delay(100);
-  #else 
-  //Ensure USB is removed
-  #warning No USB
+#else
+//Ensure USB is removed
+#warning No USB
   USBDevice.detach();
   Serial.end();
-  #endif  //RUN_WITH_USB
+#endif //RUN_WITH_USB
 
   SerialTty.println(F("\n\n---Boot Sw Build: "));
   SerialTty.println(build_ref);
-	SerialTty.print("  ***** Low Power RTC SAMD51 ");
+  SerialTty.print("  ***** Low Power RTC SAMD51 ");
   SerialTty.print(F_CPU);
   SerialTty.print("MHz ***** ");
-  #if defined RUN_WITH_USB
+#if defined RUN_WITH_USB
   SerialTty.print("USB");
-  #else
+#else
   SerialTty.print("UART");
-  #endif // RUN_WITH_USB
-  #endif // PRINT_DETAILS
+#endif // RUN_WITH_USB
+#endif // PRINT_DETAILS
 
-
-  if (!rtcIntPhy.begin()) {
-      #if PRINT_DETAILS > 0
-      SerialTty.println("Couldn't find RTC -halting");
-      #endif // PRINT_DETAILS
-      while (1) {
-          flash_redLed(10,100);
-          delay(2000);
-      }
-  }  
+  if (!rtcIntPhy.begin())
+  {
+#if PRINT_DETAILS > 0
+    SerialTty.println("Couldn't find RTC -halting");
+#endif // PRINT_DETAILS
+    while (1)
+    {
+      flash_redLed(10, 100);
+      delay(2000);
+    }
+  }
 
   updateAlarm(ALARM_UPDATE_SEC);
 
-  #if PRINT_DETAILS > 0
-	SerialTty.println("\n\rSet Alarm, starting in:");
-  #endif // PRINT_DETAILS
-	for (count=COUNT_DOWN; count > 0; count--)
-	{
-    #if PRINT_DETAILS > 0
-	  SerialTty.print(count);	
-	  SerialTty.println(" s");
-    #endif // PRINT_DETAILS
-    digitalWrite(LED1,count&0x01);
-	  delay(1000);
+#if PRINT_DETAILS > 0
+  SerialTty.println("\n\rSet Alarm, starting in:");
+#endif // PRINT_DETAILS
+  for (count = COUNT_DOWN; count > 0; count--)
+  {
+#if PRINT_DETAILS > 0
+    SerialTty.print(count);
+    SerialTty.println(" s");
+#endif // PRINT_DETAILS
+    digitalWrite(LED1, count & 0x01);
+    delay(1000);
   }
   // *********************
-    
+
   // External interrupt on pin (example: press of an active low button)
   // A pullup resistor is used to hold the signal high when no button press
   //attachInterrupt(pin, blink, LOW);
 
   print_mclk();
-	delay(100);
+  delay(100);
 
-  #if PRINT_DETAILS > 1
-  uint32_t nvicPriority= NVIC_GetPriorityGrouping();
+#if PRINT_DETAILS > 1
+  uint32_t nvicPriority = NVIC_GetPriorityGrouping();
   SerialTty.print("NVIC ");
   SerialTty.println(nvicPriority);
 
-	delay(100);
-  #endif //  PRINT_DETAILS > 1
+  delay(100);
+#endif //  PRINT_DETAILS > 1
   DWT->CTRL &= ~DWT_CTRL_CYCCNTENA_Msk;
 
-  #if PRINT_DETAILS > 1
+#if PRINT_DETAILS > 1
 
   SerialTty.print("Alm ");
   //SerialTty.print(RTC->MODE2.Mode2Alarm[RTC_ALM_ID].ALARM.reg,HEX);
   print_rtc_time_field(RTC->MODE2.Mode2Alarm[RTC_ALM_ID].ALARM.reg);
   SerialTty.print(" Match ");
   //SerialTty.print(RTC->MODE2.Mode2Alarm[RTC_ALM_ID].ALARM.reg,HEX);
-  SerialTty.print(RTC->MODE2.Mode2Alarm[RTC_ALM_ID].MASK.bit.SEL ,HEX);
+  SerialTty.print(RTC->MODE2.Mode2Alarm[RTC_ALM_ID].MASK.bit.SEL, HEX);
 
   SerialTty.print(" Ctl ");
-  SerialTty.print(RTC->MODE2.CTRLA.reg ,HEX);
+  SerialTty.print(RTC->MODE2.CTRLA.reg, HEX);
   SerialTty.println();
   SerialTty.print("Check actIRQ:");
   int intlp;
-  for (intlp=0; intlp< PERIPH_COUNT_IRQn; intlp++)
+  for (intlp = 0; intlp < PERIPH_COUNT_IRQn; intlp++)
   {
-    if (NVIC_GetEnableIRQ((IRQn_Type)intlp)) {
-        SerialTty.print(" ");
-        SerialTty.print(intlp);
+    if (NVIC_GetEnableIRQ((IRQn_Type)intlp))
+    {
+      SerialTty.print(" ");
+      SerialTty.print(intlp);
     }
   }
   SerialTty.print(" TotChecked=");
   SerialTty.println(intlp);
-  #endif //PRINT_DETAILS
+#endif //PRINT_DETAILS
 
-	delay(100);
-  digitalWrite(LED1 , LOW);
-  count =0;
+  delay(100);
+  digitalWrite(LED1, LOW);
+  count = 0;
 } //setup()
 
-
-void loop() 
+void loop()
 {
-  //int irq_mapping ;
-  #if  PRINT_DETAILS >0 
-	SerialTty.println("Entering sleep standby mode.");
-	SerialTty.println("RTC enabled  to wake the processor.");
-	SerialTty.println("Zzzz...");
-	delay(10);
-  #endif //PRINT_DETAILS
-
+//int irq_mapping ;
+#if PRINT_DETAILS > 0
+  SerialTty.println("Entering sleep standby mode.");
+  SerialTty.println("RTC enabled  to wake the processor.");
+  SerialTty.println("Zzzz...");
+  delay(10);
+#endif //PRINT_DETAILS
 
   print_mclk();
 
-  #if defined RUN_WITH_USB & (PRINT_DETAILS > 0)
-	// Detach USB interface
-	USBDevice.detach();
-  #endif //RUN_WITH_USB
+#if defined RUN_WITH_USB & (PRINT_DETAILS > 0)
+  // Detach USB interface
+  USBDevice.detach();
+#endif //RUN_WITH_USB
 
   lowpower_disable_ints();
   /* Enter SAMD51 sleep mode  see manual PM_
@@ -261,38 +261,40 @@ void loop()
    0.6 6 BACKUP Only Backup domain is powered ON. Internal Reset
    0.1 7 OFF All power domains are powered OFF. External RESET 
   */
-  LowPower.idle(SLEEP_STANDBY ); 
+  LowPower.idle(SLEEP_STANDBY);
   lowpower_enable_ints();
   //Get source of interrupt
 #if defined RUN_WITH_USB & (PRINT_DETAILS > 0)
   // Attach USB interface
   USBDevice.attach();
   // Wait for serial USB port to open
-  while(!SerialTty);
-#endif 
+  while (!SerialTty)
+    ;
+#endif
   // Serial USB is blazing fast, you might miss the messages
 
   count++;
-  #if PRINT_DETAILS > 0
+#if PRINT_DETAILS > 0
   delay(200);
   SerialTty.print(count);
   SerialTty.print(":Awake ");
   SerialTty.println(alarmUpdate_sema);
-  alarmUpdate_sema=false;
-  #endif // PRINT_DETAILS
-  delay(2000); //Allow 2secs measurement time
-  flash_redLed(5,500); //5secs medium flashing
-  digitalWrite(LED1,count&0x01);
+  alarmUpdate_sema = false;
+#endif                  // PRINT_DETAILS
+  delay(2000);          //Allow 2secs measurement time
+  flash_redLed(5, 500); //5secs medium flashing
+  digitalWrite(LED1, count & 0x01);
 
 } // loop()
 
-//PM_IRQn Power Manager can't be disabled 
+//PM_IRQn Power Manager can't be disabled
 // https://www.avrfreaks.net/forum/samd21-samd21e16b-sporadically-locks-and-does-not-wake-standby-sleep-mode
-void lowpower_disable_ints(void) {
-  #if PRINT_DETAILS > 0
+void lowpower_disable_ints(void)
+{
+#if PRINT_DETAILS > 0
   SerialTty.flush();
   SerialTty.end();
-  #endif // PRINT_DETAILS
+#endif // PRINT_DETAILS
 
   SysTick->CTRL &= ~(SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk);
 
@@ -313,21 +315,23 @@ void lowpower_disable_ints(void) {
 	//	  | MCLK_APBDMASK_TC6 | MCLK_APBDMASK_TC7 | MCLK_APBDMASK_SERCOM6 | MCLK_APBDMASK_SERCOM7);
   MCLK->APBDMASK.reg &= ~(MCLK_APBDMASK_DAC | MCLK_APBDMASK_SERCOM4 | MCLK_APBDMASK_SERCOM5 | MCLK_APBDMASK_ADC0 | MCLK_APBDMASK_ADC1 | MCLK_APBDMASK_TCC4
 		  | MCLK_APBDMASK_TC6 | MCLK_APBDMASK_TC7 | MCLK_APBDMASK_SERCOM6 | MCLK_APBDMASK_SERCOM7);/**/
-#endif 
+#endif
 
 } // lowpower_disable_ints
 
-void lowpower_enable_ints(void) {
-  SysTick_Config( SystemCoreClock / 1000 );
-  #if PRINT_DETAILS > 0 //defined PRINT_WITH_TXRX 
+void lowpower_enable_ints(void)
+{
+  SysTick_Config(SystemCoreClock / 1000);
+#if PRINT_DETAILS > 0 //defined PRINT_WITH_TXRX
   //MCLK->APBDMASK.reg |=  MCLK_APBDMASK_SERCOM5;
   SerialTty.begin(SERIAL_TTY_BAUD);
-  #endif 
-}  // lowpower_enable_ints
+#endif
+} // lowpower_enable_ints
 
-void flash_redLed(int count,int space_ms) 
+void flash_redLed(int count, int space_ms)
 {
-  for (int lpcnt=count;  lpcnt>0; lpcnt--) {
+  for (int lpcnt = count; lpcnt > 0; lpcnt--)
+  {
     digitalWrite(LED_BUILTIN, HIGH); // Show we're awake again
     delay(space_ms);
     digitalWrite(LED_BUILTIN, LOW);
@@ -335,54 +339,54 @@ void flash_redLed(int count,int space_ms)
   }
 } // flash_redLed
 
-// This is called at interrupt - 
+// This is called at interrupt -
 // Wakes up processor and then resumes normal processing
 void alarmMatch(uint32_t flag)
 {
-  alarmUpdate_sema= true;
+  alarmUpdate_sema = true;
 } // alarmMatch
 
-void updateAlarm(int update_secs) 
+void updateAlarm(int update_secs)
 {
-    DateTime now = rtcIntPhy.now();
-    DateTime alarm = DateTime(now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second() + update_secs);
+  DateTime now = rtcIntPhy.now();
+  DateTime alarm = DateTime(now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second() + update_secs);
 
-    rtcIntPhy.setAlarm(RTC_ALM_ID,alarm);
-    rtcIntPhy.attachInterrupt(alarmMatch);   
-    rtcIntPhy.enableAlarm(RTC_ALM_ID, rtcIntPhy.MATCH_SS); // match Every Second
+  rtcIntPhy.setAlarm(RTC_ALM_ID, alarm);
+  rtcIntPhy.attachInterrupt(alarmMatch);
+  rtcIntPhy.enableAlarm(RTC_ALM_ID, rtcIntPhy.MATCH_SS); // match Every Second
 } // updateAlarm
 
-void print_rtc_time_field(uint32_t time_value) 
+void print_rtc_time_field(uint32_t time_value)
 {
-  #if PRINT_DETAILS > 0
+#if PRINT_DETAILS > 0
   //SerialTtyprint("Time ");
   // Doesn't like this RtcMode2Alarm rtc_mode2alm = RTC->MODE2.Mode2Alarm[RTC_ALM_ID];
-  SerialTty.print(RTC->MODE2.Mode2Alarm[RTC_ALM_ID].ALARM.bit.YEAR );
+  SerialTty.print(RTC->MODE2.Mode2Alarm[RTC_ALM_ID].ALARM.bit.YEAR);
   SerialTty.print("/");
-  SerialTty.print(RTC->MODE2.Mode2Alarm[RTC_ALM_ID].ALARM.bit.MONTH );
+  SerialTty.print(RTC->MODE2.Mode2Alarm[RTC_ALM_ID].ALARM.bit.MONTH);
   SerialTty.print("/");
-  SerialTty.print(RTC->MODE2.Mode2Alarm[RTC_ALM_ID].ALARM.bit.DAY );
+  SerialTty.print(RTC->MODE2.Mode2Alarm[RTC_ALM_ID].ALARM.bit.DAY);
   SerialTty.print(" ");
-  SerialTty.print(RTC->MODE2.Mode2Alarm[RTC_ALM_ID].ALARM.bit.HOUR );
+  SerialTty.print(RTC->MODE2.Mode2Alarm[RTC_ALM_ID].ALARM.bit.HOUR);
   SerialTty.print(":");
-  SerialTty.print(RTC->MODE2.Mode2Alarm[RTC_ALM_ID].ALARM.bit.MINUTE );
+  SerialTty.print(RTC->MODE2.Mode2Alarm[RTC_ALM_ID].ALARM.bit.MINUTE);
   SerialTty.print(":");
-  SerialTty.print(RTC->MODE2.Mode2Alarm[RTC_ALM_ID].ALARM.bit.SECOND );        
-  #endif // PRINT_DETAILS
+  SerialTty.print(RTC->MODE2.Mode2Alarm[RTC_ALM_ID].ALARM.bit.SECOND);
+#endif // PRINT_DETAILS
 
 } //  print_rtc_time_field(
 
 void print_mclk(void)
 {
-  #if PRINT_DETAILS > 1
+#if PRINT_DETAILS > 1
   SerialTty.print("MCLK ");
-  SerialTty.print(MCLK->APBAMASK.reg,HEX);
+  SerialTty.print(MCLK->APBAMASK.reg, HEX);
   SerialTty.print(" ");
-  SerialTty.print(MCLK->APBBMASK.reg,HEX);
+  SerialTty.print(MCLK->APBBMASK.reg, HEX);
   SerialTty.print(" ");
-  SerialTty.print(MCLK->APBCMASK.reg,HEX);
+  SerialTty.print(MCLK->APBCMASK.reg, HEX);
   SerialTty.print(" ");
-  SerialTty.println(MCLK->APBDMASK.reg,HEX);
+  SerialTty.println(MCLK->APBDMASK.reg, HEX);
   delay(100); //Let print finish
-  #endif // PRINT_DETAILS
+#endif        // PRINT_DETAILS
 } // print_mclk
